7bb16ca15d68f24637b52e9454feba03a7ac5e3f
Razvan Ristea <ristearzvn@gmail.com>
2020-06-21 17:14:53 +0000

    Add outputs in template.yaml

---
+++ echo_frontend_config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "export default {"
+echo "	apiGateway: {"
+echo "		REGION: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text)',"
+echo "		URL: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendHelloApiUrl'].OutputValue" --output text)'"
+echo "	},"
+echo "	cognito: {"
+echo "		REGION: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text)',"
+echo "		USER_POOL_ID: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendCognitoUserPoolId'].OutputValue" --output text)',"
+echo "		APP_CLIENT_ID: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendCognitoUserPoolClientId'].OutputValue" --output text)',"
+echo "		DOMAIN: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendHelloApiId'].OutputValue" --output text).auth.$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text).amazoncognito.com',"
+echo "		SCOPE: ['email', 'openid'],"
+echo "		REDIRECT_SIGN_IN: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='FrontendUrl'].OutputValue" --output text)',"
+echo "		REDIRECT_SIGN_OUT: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='FrontendUrl'].OutputValue" --output text)',"
+echo "		RESPONSE_TYPE: 'code'"
+echo "	}"
+echo "};"
