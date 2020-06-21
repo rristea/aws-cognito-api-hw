a545904c271a0fab8d7fb1a64e8c6b4532980dee
Razvan Ristea <ristearzvn@gmail.com>
2020-06-21 17:55:22 +0000

    Add scripts to automatically create entire stack

--- echo_frontend_config.sh
+++ echo_frontend_config.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+STACK_NAME=$(cat samconfig.toml | grep stack_name | grep -o '"[^"]*' | tr -d '"')
+
 echo "export default {"
 echo "	apiGateway: {"
-echo "		REGION: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text)',"
-echo "		URL: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendHelloApiUrl'].OutputValue" --output text)'"
+echo "		REGION: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text)',"
+echo "		URL: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='BackendHelloApiUrl'].OutputValue" --output text)'"
 echo "	},"
 echo "	cognito: {"
-echo "		REGION: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text)',"
-echo "		USER_POOL_ID: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendCognitoUserPoolId'].OutputValue" --output text)',"
-echo "		APP_CLIENT_ID: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendCognitoUserPoolClientId'].OutputValue" --output text)',"
-echo "		DOMAIN: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='BackendHelloApiId'].OutputValue" --output text).auth.$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text).amazoncognito.com',"
+echo "		REGION: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text)',"
+echo "		USER_POOL_ID: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='BackendCognitoUserPoolId'].OutputValue" --output text)',"
+echo "		APP_CLIENT_ID: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='BackendCognitoUserPoolClientId'].OutputValue" --output text)',"
+echo "		DOMAIN: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='FrontendProxyApiId'].OutputValue" --output text).auth.$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='Region'].OutputValue" --output text).amazoncognito.com',"
 echo "		SCOPE: ['email', 'openid'],"
-echo "		REDIRECT_SIGN_IN: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='FrontendUrl'].OutputValue" --output text)',"
-echo "		REDIRECT_SIGN_OUT: '$(aws cloudformation describe-stacks --stack-name cognito-hw --query "Stacks[0].Outputs[?OutputKey=='FrontendUrl'].OutputValue" --output text)',"
+echo "		REDIRECT_SIGN_IN: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='FrontendUrl'].OutputValue" --output text)',"
+echo "		REDIRECT_SIGN_OUT: '$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='FrontendUrl'].OutputValue" --output text)',"
 echo "		RESPONSE_TYPE: 'code'"
 echo "	}"
 echo "};"
