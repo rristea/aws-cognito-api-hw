fbf56d6a9a7fb631195b7226768a1120647bdf34
Razvan Ristea <ristearzvn@gmail.com>
2020-06-21 16:21:55 +0000

    Add missing dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import OAuthButton from './OAuthButton';
+import { API, Auth, Hub } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+  	console.log('constructor');
+    super(props);
+    this.signOut = this.signOut.bind(this);
+    // let the Hub module listen on Auth events
+    Hub.listen('auth', (data) => {
+        switch (data.payload.event) {
+            case 'signIn':
+            	API.get('testApiCall', '/hello').then(response => {
+			      this.setState({responseMessage: response.message});
+			    });
+                this.setState({authState: 'signedIn', authData: data.payload.data});
+                break;
+            case 'signIn_failure':
+                this.setState({authState: 'signIn', authData: null, authError: data.payload.data});
+                break;
+            default:
+                break;
+        }
+    });
+    this.state = {
+      authState: 'loading',
+      authData: null,
+      authError: null,
+      responseMessage: 'Calling API...'
+    }
+  }
+
+  componentDidMount() {
+    console.log('on component mount');
+    // check the current user when the App component is loaded
+    Auth.currentAuthenticatedUser().then(user => {
+      console.log('signed in');
+      console.log(user);
+      API.get('testApiCall', '/hello').then(response => {
+      	this.setState({responseMessage: response.message});
+      });
+      this.setState({authState: 'signedIn'});
+    }).catch(e => {
+      console.log(e);
+      console.log('not signed in');
+      this.setState({authState: 'signIn'});
+    });
+  }
+
+  signOut() {
+    Auth.signOut().then(() => {
+      this.setState({authState: 'signIn'});
+    }).catch(e => {
+      console.log(e);
+    });
+  }
+
+  render() {
+  	console.log('render');
+    const { authState } = this.state;
+    return (
+      <div className="App">
+        {authState === 'loading' && (<div>loading...</div>)}
+        {authState === 'signIn' && <OAuthButton/>}
+        {authState === 'signedIn' && (<div><p>{this.state.responseMessage}</p><button onClick={this.signOut}>Sign out</button></div>)}
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Amplify, { Auth } from 'aws-amplify';
+
+import config from './config';
 import './index.css';
 import App from './App';
+
+Amplify.configure({
+	Auth: {
+		mandatorySignIn: true,
+		region: config.cognito.REGION,
+		userPoolId: config.cognito.USER_POOL_ID,
+		userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+		oauth: {
+			domain: config.cognito.DOMAIN,
+			scope: config.cognito.SCOPE,
+			redirectSignIn: config.cognito.REDIRECT_SIGN_IN,
+			redirectSignOut: config.cognito.REDIRECT_SIGN_OUT,
+			responseType: config.cognito.RESPONSE_TYPE
+		}
+	},
+	API: {
+		endpoints: [
+			{
+				name: 'testApiCall',
+				endpoint: config.apiGateway.URL,
+				region: config.apiGateway.REGION,
+				custom_header: async () => {
+					return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
+				}
+			}
+		]
+	}
+});
 
 ReactDOM.render(
   <React.StrictMode>
